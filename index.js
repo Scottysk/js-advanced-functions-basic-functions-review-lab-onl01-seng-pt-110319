386a9623a80140edecf9cd124eff929830107656
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-06-22 22:54:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-function saturdayFun(saturday = "roller-skate") {
-  return `This Saturday, I want to ${saturday}!`;
+function saturdayFun(fun = "roller-skate") {
+  return `This Saturday, I want to ${fun}!`;
 }
 
 function mondayWork(work = "go to the office") {
